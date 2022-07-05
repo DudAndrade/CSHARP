4094d68114f2985114503f81effb0fde2922a4db
DudAndrade <maripereira1818@gmail.com>
2022-07-05 16:56:30 -0300

    Atualzado com o método e alguns comentários explicativos do código

--- Duda/CSharp/PrimeiroProjeto/ContaCorrente.cs
+++ Duda/CSharp/PrimeiroProjeto/ContaCorrente.cs
@@ -1,16 +1,24 @@
 public class ContaCorrente     //classe pública 
 {
-    public string Titular;
+    public string Titular; // esses são os atributos, ou seja, características da classe
     public int Agencia;
     public int Numero;
     public double Saldo;
-    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo)
+    public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo) // declarando o construtor  SEMPRE DENTRO DA CLASSE
     { 
-        this.Titular = Titular;
-        this.Agencia = Agencia;
+        this.Titular = Titular; // representa tal característica da classe
+        this.Agencia = Agencia; 
         this.Numero = Numero;
         this.Saldo = Saldo; 
-         }
+     }
+
+     public bool Sacar(double valorSaque){ // - bool(true ou false) - aqui está sendo declarado o método, ele sempre tem o tipo de retorno declarado
+        if (this.Saldo >= valorSaque){
+            this.Saldo = this.Saldo - valorSaque;
+            return true;
+        }
+        return false;
+     }
 }
    
 
--- Duda/CSharp/PrimeiroProjeto/Program.cs
+++ Duda/CSharp/PrimeiroProjeto/Program.cs
@@ -7,21 +7,31 @@ namespace meuPrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta_da_barbara = new ContaCorrente ("Barbara", 12345, 789, 10);    // variável do tipo conta corrente 
-            
+            ContaCorrente conta_da_barbara = new ContaCorrente ("Barbara", 12345, 789, 10);   // criando os objetos da classe
+            ContaCorrente conta_da_mariana = new ContaCorrente ("Mariana", 12345, 224, 150);   
+            ContaCorrente conta_do_sanches = new ContaCorrente ("Sanches", 12345, 668, 300);
+
             Console.WriteLine ("A conta é do(a) " + conta_da_barbara.Titular + ", a agência é "
              + conta_da_barbara.Agencia + " e o número é " + conta_da_barbara.Numero + ".");
-// ____________________________________________________________________________________________________
-            ContaCorrente conta_da_mariana = new ContaCorrente ("Mariana", 12345, 224, 21);    // variável do tipo conta corrente 
-        
+
             Console.WriteLine ("A conta é do(a) " + conta_da_mariana.Titular + ", a agência é "
              + conta_da_mariana.Agencia + " e o número é " + conta_da_mariana.Numero + ".");
-//____________________________________________________________________________________________________
-            ContaCorrente conta_da_sanches = new ContaCorrente ("Sanches", 12345, 668, 26);    // variável do tipo conta corrente 
 
-            Console.WriteLine ("A conta é do(a) " + conta_da_sanches.Titular + ", a agência é "
-             + conta_da_sanches.Agencia + " e o número é " + conta_da_sanches.Numero + ".");
+            Console.WriteLine ("A conta é do(a) " + conta_do_sanches.Titular + ", a agência é "
+             + conta_do_sanches.Agencia + " e o número é " + conta_do_sanches.Numero + ".");
 
+             bool sacar_conta = conta_da_barbara.Sacar(100);
+             bool sacar_conta1 = conta_da_mariana.Sacar(100);
+             bool sacar_conta2 = conta_do_sanches.Sacar(100);
+
+             Console.WriteLine ("A conta é do(a) " + conta_da_barbara.Titular + ", a agência é "
+             + conta_da_barbara.Agencia + " e o número é " + conta_da_barbara.Numero + " Saldo " + conta_da_barbara.Saldo + ".");
+
+            Console.WriteLine ("A conta é do(a) " + conta_da_mariana.Titular + ", a agência é "
+             + conta_da_mariana.Agencia + " e o número é " + conta_da_mariana.Numero + " Saldo " + conta_da_mariana.Saldo + ".");
+
+            Console.WriteLine ("A conta é do(a) " + conta_do_sanches.Titular + ", a agência é "
+             + conta_do_sanches.Agencia + " e o número é " + conta_do_sanches.Numero + " Saldo " + conta_do_sanches.Saldo + ".");
         }
     }    
 }
